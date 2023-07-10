fa465b5acdd296d00b0c92b4bd3b55e4c9bac6d7
Abdul Nahid <amnahid03@gmail.com>
2023-07-10 23:40:19 +0530

    first commit

---
+++ client/src/constants/ApiList.js
@@ -0,0 +1,21 @@
+
+
+const localhostallData = "http://localhost:4040"
+
+
+const localhostSignup ="http://localhost:4040/user/signup"
+const localhostLogin ="http://localhost:4040/user/login"
+const localhostAuth ="http://localhost:4040/user/auth"
+const localhostLogout = "http://localhost:4040/user/logout"
+
+const SignupApi = "https://blog-server-sknv.onrender.com/user/signup"
+const LoginApi = "https://blog-server-sknv.onrender.com/user/login"
+const AuthApi= "https://blog-server-sknv.onrender.com/user/auth"
+const LogoutApi = "https://blog-server-sknv.onrender.com/user/logout"
+
+const AllData = "https://blog-server-sknv.onrender.com"
+
+
+
+
+export {localhostallData,localhostAuth, localhostLogout,localhostLogin, localhostSignup, SignupApi, LoginApi, AuthApi,LogoutApi , AllData}
